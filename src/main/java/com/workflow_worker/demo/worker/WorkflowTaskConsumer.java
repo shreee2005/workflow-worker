3ca69b1c6f59360caa9fe975f94b6ec59be2c8c1
shriraj <jagtapshriraj6@gmail.com>
2026-02-27 19:04:20 +0530

    OOOooo

--- src/main/java/com/workflow_worker/demo/worker/WorkflowTaskConsumer.java
+++ src/main/java/com/workflow_worker/demo/worker/WorkflowTaskConsumer.java
@@ -1,36 +1,24 @@
 package com.workflow_worker.demo.worker;
-
 import com.workflow_worker.demo.engine.WorkflowStepEngine;
 import com.workflow_worker.demo.engine.lock.DistributedLockService;
 import com.workflow_worker.demo.engine.retry.RetryService;
-import com.workflow_worker.demo.entity.Workflow;
-import com.workflow_worker.demo.entity.WorkflowRun;
-import com.workflow_worker.demo.entity.WorkflowRunStep;
 import com.workflow_worker.demo.executers.StepExecutorRegistry;
-import com.workflow_worker.demo.messaging.WorkflowDlqMessage;
 import com.workflow_worker.demo.messaging.WorkflowJobMessage;
 import com.workflow_worker.demo.repository.WorkflowRepository;
 import com.workflow_worker.demo.service.WorkflowRunService;
 import com.workflow_worker.demo.service.WorkflowRunStepService;
-import com.workflow_worker.demo.workflow.StepDefinition;
-import com.workflow_worker.demo.workflow.WorkflowSpecParser;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
-
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Component
 public class WorkflowTaskConsumer {
     private final DistributedLockService distributedLockService;
     private final RetryService retryService;
-    private final WorkflowRepository workflowRepository;
     private final WorkflowStepEngine stepEngine;
     private final WorkflowRunService workflowRunService;
-    private final WorkflowRunStepService stepService;
     private final WorkflowMetrics metrics;
-    private final RabbitTemplate rabbitTemplate;
 
     public WorkflowTaskConsumer(
             DistributedLockService distributedLockService, WorkflowRepository workflowRepository,
@@ -41,13 +29,12 @@ public class WorkflowTaskConsumer {
             RabbitTemplate rabbitTemplate
     ) {
         this.distributedLockService = distributedLockService;
-        this.workflowRepository = workflowRepository;
+
         this.workflowRunService = workflowRunService;
-        this.stepService = stepService;
         this.retryService = retryService;
         this.stepEngine = stepEngine;
         this.metrics = metrics;
-        this.rabbitTemplate = rabbitTemplate;
+
     }
 
     @RabbitListener(queues = "workflow.tasks")
@@ -64,7 +51,6 @@ public class WorkflowTaskConsumer {
             workflowRunService.markRunning(runId);
             metrics.runsStarted.increment();
         } catch (IllegalStateException e) {
-            // duplicate / already finished – do nothing, just exit
             return;
         }
         try{
@@ -86,7 +72,6 @@ public class WorkflowTaskConsumer {
             );
         }
         finally {
-            // Always release distributed lock
             distributedLockService.release(runId);
         }
     }
