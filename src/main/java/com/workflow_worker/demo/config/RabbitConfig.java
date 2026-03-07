94f217c216cc13a4e3838658706aaedb2f9f5475
shriraj <jagtapshriraj6@gmail.com>
2026-03-07 22:24:55 +0530

    Retry Logic Fixed

--- src/main/java/com/workflow_worker/demo/config/RabbitConfig.java
+++ src/main/java/com/workflow_worker/demo/config/RabbitConfig.java
@@ -5,8 +5,8 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
